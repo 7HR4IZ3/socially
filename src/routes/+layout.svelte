a9a01bdd6c44d9d5106b22938876c5ab5588fbf6
7HR4IZ3 <gamerxville@gmail.com>
2024-10-09 00:51:13 -0700

    feat: added extra routes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,13 @@
 <script>
     import "../app.css";
+    import Header from "../components/Header.svelte";
+    import Sidebar from "../components/Sidebar.svelte";
 </script>
 
-<div class="app">
-    <slot></slot>
+<div class="bg-muted/40 flex min-h-screen w-full flex-col">
+    <Sidebar />
+    <div class="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+        <Header />
+       <slot></slot>
+    </div>
 </div>
-
-<style>
-</style>
---
+++ src/routes/settings/integrations/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+    
+    export const data: PageData = {};
+</script>
